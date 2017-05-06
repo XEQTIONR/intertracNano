e5fe5112a20c3b238c5801968e3ca826acfc001d
Ishtehar Hussain <ishteharhussain@gmail.com>
2017-05-06 01:13:53 -0400

    Modified the blade file to reflect changes in database table. Changes are not required for new_customer.

--- resources/views/customers.blade.php
+++ resources/views/customers.blade.php
@@ -6,17 +6,23 @@
   <tr>
     <th>customer#</th>
     <th>Customer Name</th>
+    <th>Address</th>
     <th>Phone</th>
     <th>Notes</th>
+    <th>Created</th>
+    <th>Updated</th>
   </tr>
 
 
   @foreach ($customers as $customer)
-    <tr>
+  <tr>
     <td>{{$customer->id}}</td>
     <td>{{$customer->name}}</td>
+    <td>{{$customer->address}}</td>
     <td>{{$customer->phone}}</td>
     <td>{{$customer->notes}}</td>
+    <td>{{$customer->created_at}}</td>
+    <td>{{$customer->updated_at}}</td>
   </tr>
   @endforeach
 
