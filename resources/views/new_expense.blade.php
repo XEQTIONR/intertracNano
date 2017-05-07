c639e700589cf3b46ddd0d774a8b758af6b3e479
Ishtehar Hussain <ishteharhussain@gmail.com>
2017-05-07 00:36:59 -0400

    new_expenses blade file missing from last commit.

---
+++ resources/views/new_expense.blade.php
@@ -0,0 +1,21 @@
+@extends('layout')
+
+
+@section('content')
+
+<span>NEW EXPENSE</span>
+
+<form method="post" action="/consignment_expenses">
+
+  {{ csrf_field() }}
+
+  BOL# <input type="text" name="inputBOL"> <br>
+  Expense Foreign <input type="text"  name="inputExpenseForeign"> <br>
+  Expense Local<input type="text" name="inputExpenseLocal"> <br>
+  Notes  <input type="text" name="inputNote"> <br>
+
+  <button type="submit" value="submit">Submit</button>
+
+</form>
+
+@endsection
