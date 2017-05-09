e7376fee3812a39d767d3a3eaff6c489bfbc3fff
Ishtehar Hussain <ishteharhussain@gmail.com>
2017-05-09 16:24:56 -0400

    Added placeholder text to item fields in new_performa_invoice.

--- resources/views/new_performa_invoice.blade.php
+++ resources/views/new_performa_invoice.blade.php
@@ -8,6 +8,7 @@
     function addItem()
     {
 
+      //names of parameters.
       var itemId = "tyre" + count;
       var qty = "qty" + count;
       var price = "price" + count;
@@ -15,36 +16,40 @@
       var subDiv = document.createElement("DIV");
       var subDivId = "subDiv" + count;
 
+      //Div for every new invice item
       subDiv.setAttribute("class", "tyreDiv");
       subDiv.setAttribute("id", subDivId);
 
       //document.getElementById("itemList").append(subDiv);
-
+      //Items that go inside the Div
       var itemInput = document.createElement("INPUT");
       itemInput.setAttribute("type", "text");
       itemInput.setAttribute("class", "input");
       itemInput.setAttribute("name", itemId);
+      itemInput.setAttribute("placeholder", "Tyre ID");
 
       //document.getElementById(subDivNum).appendChild(itemInput);
       $("#"+subDivId).append("Tyre ID: ");
-      subDiv.appendChild(itemInput);
+      subDiv.appendChild(itemInput); //insert in the Div
 
       var qtyInput = document.createElement("INPUT");
       qtyInput.setAttribute("type", "text");
       qtyInput.setAttribute("class", "input");
       qtyInput.setAttribute("name", qty);
+      qtyInput.setAttribute("placeholder", "Quantity");
 
       $("#"+subDivId).append("Quantity: ");
-      subDiv.appendChild(qtyInput);
+      subDiv.appendChild(qtyInput); //insert in the Div
       //document.getElementById(subDivNum).appendChild(qtyInput);
 
       var priceInput = document.createElement("INPUT");
       priceInput.setAttribute("type", "text");
       priceInput.setAttribute("class", "input");
       priceInput.setAttribute("name", price);
+      priceInput.setAttribute("placeholder", "Unit Price");
 
       $("#"+subDivId).append("Unit Price: ");
-      subDiv.appendChild(priceInput);
+      subDiv.appendChild(priceInput); //insert in the Div
       //document.getElementById(subDivNum).appendChild(priceInput);
 
       var itemlist = document.getElementById("itemList");
