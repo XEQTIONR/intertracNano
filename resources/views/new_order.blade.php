041daaa22fb3b0b0d0124c48c921c50c450dde32
Ishtehar Hussain <ishteharhussain@gmail.com>
2017-05-30 03:11:05 -0400

    Moved current stock table to a partial. Expects array  with fields tyre_id and in_stock.

--- resources/views/new_order.blade.php
+++ resources/views/new_order.blade.php
@@ -103,20 +103,6 @@
 </div> <!-- leftDiv -->
 
 <div class="rightDiv">
-    <table class="DBinfo">
-      <tr>
-        <th> Tyre ID </th>
-        <th> #remaining </th>
-      </tr>
-      @foreach ($in_stock as $item)
-        <tr>
-
-          <td>{{$item->tyre_id}}</td>
-          <td>{{$item->in_stock}}</td>
-
-        </tr>
-      @endforeach
-
-    </table>
+  @include('partials.currentstock');
 </div>
 @endsection
