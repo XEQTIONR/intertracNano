3d9df0aa89bbb148161747c39a839663bc8920ef
Ishtehar Hussain <ishteharhussain@gmail.com>
2017-06-04 14:14:09 -0400

    Moved add and remove item scripts to js directory.

--- resources/views/new_order.blade.php
+++ resources/views/new_order.blade.php
@@ -14,66 +14,7 @@
     }
 
   </style>
-  <script>
-    // javascript included in layout file.
-    var count=0;
-    function addItem()
-    {
-
-      //names of parameters.
-      var itemId = "tyre[" + count + "]";
-      var qty = "qty[" + count + "]";
-      var price = "price[" + count + "]";
-
-      var subDiv = document.createElement("DIV");
-      var subDivId = "subDiv" + count;
-
-      //Div for every new invice item
-      subDiv.setAttribute("class", "tyreDiv");
-      subDiv.setAttribute("id", subDivId);
-
-      //document.getElementById("itemList").append(subDiv);
-      //Items that go inside the Div
-      var itemInput = document.createElement("INPUT");
-      itemInput.setAttribute("type", "text");
-      itemInput.setAttribute("class", "input");
-      itemInput.setAttribute("name", itemId);
-      itemInput.setAttribute("placeholder", "Tyre ID");
-
-      //document.getElementById(subDivNum).appendChild(itemInput);
-      $("#"+subDivId).append("Tyre ID: ");
-      subDiv.appendChild(itemInput); //insert in the Div
-
-      var qtyInput = document.createElement("INPUT");
-      qtyInput.setAttribute("type", "text");
-      qtyInput.setAttribute("class", "input");
-      qtyInput.setAttribute("name", qty);
-      qtyInput.setAttribute("placeholder", "Quantity");
-
-      $("#"+subDivId).append("Quantity: ");
-      subDiv.appendChild(qtyInput); //insert in the Div
-      //document.getElementById(subDivNum).appendChild(qtyInput);
-
-      var priceInput = document.createElement("INPUT");
-      priceInput.setAttribute("type", "text");
-      priceInput.setAttribute("class", "input");
-      priceInput.setAttribute("name", price);
-      priceInput.setAttribute("placeholder", "Unit Price");
-
-      $("#"+subDivId).append("Unit Price: ");
-      subDiv.appendChild(priceInput); //insert in the Div
-      //document.getElementById(subDivNum).appendChild(priceInput);
-
-      var itemlist = document.getElementById("itemList");
-
-      itemlist.appendChild(subDiv);
-
-      count++;
-      document.getElementById("numItems").value = count;
-    }
-
-
-  </script>
+  <script src="/js/addItem.js"></script>
 @endsection
 
 @section('content')
@@ -91,6 +32,7 @@
 
   <button type="button" onclick="addItem()">Add New Item</button>
   <button type="submit" value="submit">Submit</button>
+  <button type="button" onclick="removeItem()">Remove Last Item</button>
 
   <div id="itemList" style="border: 2px dashed black;">
 
