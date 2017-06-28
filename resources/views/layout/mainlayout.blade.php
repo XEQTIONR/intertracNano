24419e889fd80e6e47594783a2bc35b9a2f6a250
Ishtehar Hussain <ishteharhussain@gmail.com>
2017-06-28 18:25:25 -0400

    Only Admins register new users.

--- resources/views/layout/mainlayout.blade.php
+++ resources/views/layout/mainlayout.blade.php
@@ -76,8 +76,9 @@
                     <!-- Authentication Links -->
                     @if (Auth::guest())
                         <li><a href="{{ route('login') }}">Login</a></li>
-                        <li><a href="{{ route('register') }}">Register</a></li>
+
                     @else
+
                         <li class="dropdown">
                             <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
                                 {{ Auth::user()->name }} <span class="caret"></span>
@@ -87,8 +88,9 @@
 
                               @if(Auth::user()->admin)
                                 <li>
-                                    I AM AN ADMIN YAY  
+                                    I AM AN ADMIN YAY
                                 </li>
+                                <li><a href="{{ route('register') }}">Register</a></li>
                               @endif
                                 <li>
                                     <a href="{{ route('logout') }}"
