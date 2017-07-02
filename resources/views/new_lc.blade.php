1ccc1b94c68fd22e2604adec3644c46fc4f41f32
Ishtehar Hussain <ishteharhussain@gmail.com>
2017-07-02 16:14:58 -0400

    Cleaned up new_lc.

--- resources/views/new_lc.blade.php
+++ resources/views/new_lc.blade.php
@@ -25,17 +25,17 @@ $(function() {
         $('#load').append('<img style="position: absolute; left: 0; top: 0; z-index: 100000;" src="/images/spinner.gif" />');
 
         var url = $(this).attr('href');
-        getArticles(url);
+        getTyres(url);
         window.history.pushState("", "", url);
     });
 
-    function getArticles(url) {
+    function getTyres(url) {
         $.ajax({
             url : url
         }).done(function (data) {
             $('#tyreCatalog').html(data);
         }).fail(function () {
-            alert('Articles could not be loaded.');
+            alert('Tyres Catalog could not be loaded.');
         });
     }
 });
@@ -53,158 +53,163 @@ $(function() {
 
 <div class="container">
   <div class="row">
-  <div class="col-md-10 col-md-offset-1">
+    <div class="col-md-10 col-md-offset-1">
 
-<form class="form-horizontal" method="post" action="/lcs">
+      <form class="form-horizontal" method="post" action="/lcs">
+        {{ csrf_field() }}
 
-  {{ csrf_field() }}
+        @if ($errors->any())
+          <div class="alert alert-danger">
+            <ul>
+              @foreach ($errors->all() as $error)
+                <li>{{$error}}</li>
+              @endforeach
+            </ul>
+          </div>
+        @endif
 
-  @if ($errors->any())
-    <div class="alert alert-danger">
-    <ul>
-      @foreach ($errors->all() as $error)
-        <li>{{$error}}</li>
-      @endforeach
-    </ul>
-    </div>
-  @endif
+        <div class="form-group">
+          <label for="LcNumber" class="col-md-3 col-md-offset-2 control-label">LC#</label>
+          <div class="col-md-3">
+            <input type="text" class="form-control" name="LcNumber" id="inputLCnum" value="{{old('LcNumber')}}">
+          </div>
+        </div>
 
+        <div class="form-group">
+          <label for="DateIssued" class="col-md-3 col-md-offset-2 control-label">Date Issued</label>
+          <div class="col-md-2">
+            <input type="text"  class="form-control pickdate" name="DateIssued" id="inputDateIssue" value="{{old('DateIssued')}}">
+          </div>
+        </div>
 
-  <div class="form-group">
-    <label for="LcNumber" class="col-md-3 col-md-offset-2 control-label">LC#</label>
-    <div class="col-md-3">
-      <input type="text" class="form-control" name="LcNumber" id="inputLCnum" value="{{old('LcNumber')}}">
-    </div>
-  </div>
+        <div class="form-group">
+          <label for="DateExpiry" class="col-md-3 col-md-offset-2 control-label">Date Expiry</label>
+          <div class="col-md-2">
+            <input type="text" class="form-control pickdate" name="DateExpiry" id="inputDateExpiry" value="{{old('DateExpiry')}}">
+          </div>
+        </div>
 
-  <div class="form-group">
-    <label for="DateIssued" class="col-md-3 col-md-offset-2 control-label">Date Issued</label>
-    <div class="col-md-2">
-      <input type="text"  class="form-control pickdate" name="DateIssued" id="inputDateIssue" value="{{old('DateIssued')}}">
-    </div>
-  </div>
+        <div class="form-group">
+          <label for="Applicant" class="col-md-3 col-md-offset-2 control-label">Applicant</label>
+          <div class="col-md-3">
+            <input type="text" class="form-control" name="Applicant" id="inputApplicant" value="{{old('Applicant')}}">
+          </div>
+        </div>
 
-  <div class="form-group">
-    <label for="DateExpiry" class="col-md-3 col-md-offset-2 control-label">Date Expiry</label>
-    <div class="col-md-2">
-      <input type="text" class="form-control pickdate" name="DateExpiry" id="inputDateExpiry" value="{{old('DateExpiry')}}">
-    </div>
-  </div>
+        <div class="form-group">
+          <label for="Beneficiary" class="col-md-3 col-md-offset-2 control-label">Beneficiary</label>
+          <div class="col-md-3">
+            <input type="text" class="form-control" name="Beneficiary" id="inputBeneficiary" value="{{old('Beneficiary')}}">
+          </div>
+        </div>
 
-  <div class="form-group">
-    <label for="Applicant" class="col-md-3 col-md-offset-2 control-label">Applicant</label>
-    <div class="col-md-3">
-      <input type="text" class="form-control" name="Applicant" id="inputApplicant" value="{{old('Applicant')}}">
-    </div>
-  </div>
+        <div class="form-group">
+          <label for="PortDepart" class="col-md-3 col-md-offset-2 control-label">Departing Port</label>
+          <div class="col-md-3">
+            <input type="text" class="form-control" name="PortDepart" id="inputPortDepart" value="{{old('PortDepart')}}">
+          </div>
+        </div>
 
-  <div class="form-group">
-    <label for="Beneficiary" class="col-md-3 col-md-offset-2 control-label">Beneficiary</label>
-    <div class="col-md-3">
-      <input type="text" class="form-control" name="Beneficiary" id="inputBeneficiary" value="{{old('Beneficiary')}}">
-    </div>
-  </div>
+        <div class="form-group">
+          <label for="PortArrive" class="col-md-3 col-md-offset-2 control-label">Arrival Port</label>
+          <div class="col-md-3">
+            <input type="text" class="form-control" name="PortArrive" id="inputPortArrive" value="{{old('PortArrive')}}">
+          </div>
+        </div>
 
-  <div class="form-group">
-    <label for="PortDepart" class="col-md-3 col-md-offset-2 control-label">Departing Port</label>
-    <div class="col-md-3">
-      <input type="text" class="form-control" name="PortDepart" id="inputPortDepart" value="{{old('PortDepart')}}">
-    </div>
-  </div>
+        <div class="form-group">
+          <label for="Invoice" class="col-md-3 col-md-offset-2 control-label">Invoice#</label>
+          <div class="col-md-3">
+            <input type="text" class="form-control" name="Invoice" id="inputInvoice" value="{{old('Invoice')}}">
+          </div>
+        </div>
 
-  <div class="form-group">
-    <label for="PortArrive" class="col-md-3 col-md-offset-2 control-label">Arrival Port</label>
-    <div class="col-md-3">
-      <input type="text" class="form-control" name="PortArrive" id="inputPortArrive" value="{{old('PortArrive')}}">
-    </div>
-  </div>
+        <div class="form-group">
+          <label for="CurrencyCode" class="col-md-3 col-md-offset-2 control-label">Foreign Currency Code</label>
+          <div class="col-md-2">
+            <input type="text" class="form-control" name="CurrencyCode" id="inputCurrencyCode" value="{{old('CurrencyCode')}}">
+          </div>
+        </div>
 
-  <div class="form-group">
-    <label for="Invoice" class="col-md-3 col-md-offset-2 control-label">Invoice#</label>
-    <div class="col-md-3">
-      <input type="text" class="form-control" name="Invoice" id="inputInvoice" value="{{old('Invoice')}}">
-    </div>
-  </div>
+        <div class="form-group">
+          <label for="ExchangeRate" class="col-md-3 col-md-offset-2 control-label">Exchange Rate</label>
+          <div class="col-md-3">
+            <input type="text" class="form-control" name="ExchangeRate" id="inputExchangeRate" value="{{old('ExchangeRate')}}">
+          </div>
+        </div>
 
-  <div class="form-group">
-    <label for="CurrencyCode" class="col-md-3 col-md-offset-2 control-label">Foreign Currency Code</label>
-    <div class="col-md-2">
-      <input type="text" class="form-control" name="CurrencyCode" id="inputCurrencyCode" value="{{old('CurrencyCode')}}">
-    </div>
-  </div>
+        <div class="form-group">
+          <label for="Value" class="col-md-3 col-md-offset-2 control-label">LC Value (Foreign Amount)</label>
+          <div class="col-md-3">
+            <input type="text" class="form-control" name="Value" id="inputValue" value="{{old('Value')}}">
+          </div>
+        </div>
 
-  <div class="form-group">
-    <label for="ExchangeRate" class="col-md-3 col-md-offset-2 control-label">Exchange Rate</label>
-    <div class="col-md-3">
-      <input type="text" class="form-control" name="ExchangeRate" id="inputExchangeRate" value="{{old('ExchangeRate')}}">
-    </div>
-  </div>
+        <div class="form-group">
+          <label for="ForeignExpense" class="col-md-3 col-md-offset-2 control-label">Expenses Paid (Foreign)</label>
+          <div class="col-md-3">
+            <input type="text" class="form-control" name="ForeignExpense" id="inputForeignExpense" value="{{old('ForeignExpense')}}">
+          </div>
+        </div>
 
-  <div class="form-group">
-    <label for="Value" class="col-md-3 col-md-offset-2 control-label">LC Value (Foreign Amount)</label>
-    <div class="col-md-3">
-      <input type="text" class="form-control" name="Value" id="inputValue" value="{{old('Value')}}">
-    </div>
-  </div>
+        <div class="form-group">
+          <label for="LocalExpense" class="col-md-3 col-md-offset-2 control-label">Expenses Paid (Local)</label>
+          <div class="col-md-3">
+            <input type="text" class="form-control" name="LocalExpense" id="inputLocalExpense" value="{{old('LocalExpense')}}">
+          </div>
+        </div>
 
-  <div class="form-group">
-    <label for="ForeignExpense" class="col-md-3 col-md-offset-2 control-label">Expenses Paid (Foreign)</label>
-    <div class="col-md-3">
-      <input type="text" class="form-control" name="ForeignExpense" id="inputForeignExpense" value="{{old('ForeignExpense')}}">
-    </div>
-  </div>
+        <div class="form-group">
+          <label for="Notes" class="col-md-3 col-md-offset-2 control-label">Notes</label>
+          <div class="col-md-3">
+            <textarea rows="5" class="form-control" name="Notes" id="inputNotes" >{{old('Notes')}}</textarea>
+          </div>
+        </div>
 
-  <div class="form-group">
-    <label for="LocalExpense" class="col-md-3 col-md-offset-2 control-label">Expenses Paid (Local)</label>
-    <div class="col-md-3">
-      <input type="text" class="form-control" name="LocalExpense" id="inputLocalExpense" value="{{old('LocalExpense')}}">
-    </div>
-  </div>
-
-  <div class="form-group">
-    <label for="Notes" class="col-md-3 col-md-offset-2 control-label">Notes</label>
-    <div class="col-md-3">
-      <textarea rows="5" class="form-control" name="Notes" id="inputNotes" >{{old('Notes')}}</textarea>
-    </div>
-  </div>
-
-
-
-
-  <button class="btn btn-success" type="button" onclick="addItem()">Add New Item</button>
-  <button class="btn btn-danger" type="button" onclick="removeItem()">Remove Last Item</button>
-
-  <button class="btn btn-info" type="submit" value="submit">Submit</button><br><br><br>
+        <button class="btn btn-success" type="button" onclick="addItem()">Add New Item</button>
+        <button class="btn btn-danger" type="button" onclick="removeItem()">Remove Last Item</button>
+        <button class="btn btn-info" type="submit" value="submit">Submit</button><br><br><br>
 
 
+        <div class="well col-md-6">
 
-  <div id="itemList" class="well col-md-6">
-    <h4>Enter Performa Invoice</h4><br>
+          <div id="itemList" class="row">
+            <h4>Enter Performa Invoice</h4>
+            <span>None of the fields can be blank</span>
+          </div><!--row-->
 
-
-  </div>
-
-  <div id="tyreCatalog" class="col-md-6">
-    {{--<h4>Tyre Catalog</h4> <br>--}}
-    @include('partials.tyres')
-  </div>
+          <div class="form-group row">
+            <label for="numItems" class="col-md-4 control-label">Num items</label>
+            <div class="col-md-4">
+              <input type="text" class="form-control" name="numItems" id="numItems" placeholder="0" readonly>
+            </div>
+          </div>
 
 
 
+        </div> <!--well col-md-6-->
 
 
-</div>
-</div>
+        <div id=catalogContainer class="col-md-6">
+          <div id="row">
+            <h4>Tyre Catalog</h4>
+          </div> <!--row-->
 
-<div class="form-group row">
-  <label for="numItems" class="col-md-2 col-md-offset-1 control-label">Num items</label>
-  <div class="col-md-1">
-    <input type="text" class="form-control" name="numItems" id="numItems" placeholder="0" readonly>
-  </div>
-</div>
-
-</form>
+          <div id="row">
+            <div id="tyreCatalog" class="col-md-12">
+              @include('partials.tyres')
+            </div>
+          </div> <!--row-->
+        </div> <!--catalogContainer-->
 
 
-</div>
+
+
+
+
+
+      </form>
+    </div> <!--col-->
+  </div> <!--row-->
+</div> <!--container-->
 @endsection
