c427f6b31981143c6d4b05d0aa0871b96a1b2db3
Ishtehar Hussain <ishteharhussain@gmail.com>
2017-07-06 04:17:42 -0400

    Added icons to the main menu

--- resources/views/layout/mainlayout.blade.php
+++ resources/views/layout/mainlayout.blade.php
@@ -35,87 +35,76 @@
 <body style="padding-top: 100px;">
 
 <div class="flex-center position-ref full-height">
-<!--@if (Route::has('login'))
-  <div class="top-right links">
-  @if (Auth::check())
-    <a href="{{ url('/home') }}">Home</a>
-  @else
-    <a href="{{ url('/login') }}">Login</a>
-    <a href="{{ url('/register') }}">Register</a>
-  @endif
-  </div>
-@endif-->
 
-    <nav class="navbar navbar-default navbar-fixed-top">
-        <div class="container">
-            <div class="navbar-header">
+  <nav class="navbar navbar-default navbar-fixed-top">
+    <div class="container">
+      <div class="navbar-header">
 
-                <!-- Collapsed Hamburger -->
-                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#app-navbar-collapse">
-                    <span class="sr-only">Toggle Navigation</span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                </button>
+        <!-- Collapsed Hamburger -->
+        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#app-navbar-collapse">
+          <span class="sr-only">Toggle Navigation</span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+        </button>
 
-                <!-- Branding Image -->
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
-                </a>
-            </div>
+        <!-- Branding Image -->
+        <a class="navbar-brand" href="{{ url('/') }}">
+          {{ config('app.name', 'Laravel') }}
+        </a>
+      </div>
 
-            <div class="collapse navbar-collapse" id="app-navbar-collapse">
-                <!-- Left Side Of Navbar -->
-                <ul class="nav navbar-nav">
-                    &nbsp;
-                </ul>
+      <div class="collapse navbar-collapse" id="app-navbar-collapse">
+        <!-- Left Side Of Navbar -->
+        <ul class="nav navbar-nav">
+          &nbsp;
+        </ul>
 
-                <!-- Right Side Of Navbar -->
-                <ul class="nav navbar-nav navbar-right">
-                    <!-- Authentication Links -->
-                    @if (Auth::guest())
-                        <li><a href="{{ route('login') }}">Login</a></li>
+        <!-- Right Side Of Navbar -->
+        <ul class="nav navbar-nav navbar-right">
+          <!-- Authentication Links -->
+          @if (Auth::guest())
+            <li><a href="{{ route('login') }}">Login</a></li>
 
-                    @else
+          @else
+            <li class="dropdown" >
+              <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
+                {{ Auth::user()->name }} <span class="caret"></span>
+              </a>
 
-                        <li class="dropdown" >
-                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
-                                {{ Auth::user()->name }} <span class="caret"></span>
-                            </a>
+              <ul class="dropdown-menu" role="menu">
 
-                            <ul class="dropdown-menu" role="menu">
+                @if(Auth::user()->admin)
+                  <li>
+                    <span>Admin Actions</span>
+                  </li>
+                  <li><a href="{{ route('register') }}">Register</a></li>
 
-                              @if(Auth::user()->admin)
-                                <li>
-                                    <span>Admin Actions</span>
-                                </li>
-                                <li><a href="{{ route('register') }}">Register</a></li>
-                                <hr>
-                              @endif
-                                <li>
-                                    <a href="{{ route('logout') }}"
-                                        onclick="event.preventDefault();
-                                                 document.getElementById('logout-form').submit();">
-                                        Logout
-                                    </a>
+                  <hr>
+                @endif
+                <li>
+                  <a href="{{ route('logout') }}"
+                    onclick="event.preventDefault();
+                             document.getElementById('logout-form').submit();">
+                            Logout
+                  </a>
 
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                        {{ csrf_field() }}
-                                    </form>
-                                </li>
-                            </ul>
-                        </li>
-                    @endif
-                </ul>
-            </div>
-        </div>
-    </nav>
+                  <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                  {{ csrf_field() }}
+                  </form>
+                </li>
+              </ul>
+            </li>
+          @endif
+        </ul>
+      </div>
+    </div>
+  </nav>
+
   @if(Auth::check())
     @include('partials.maintabsmenu')
     @include('partials.mainmenu')
   @endif
-
-
 
 </div>
 
--- resources/views/partials/mainmenu.blade.php
+++ resources/views/partials/mainmenu.blade.php
@@ -21,16 +21,83 @@
 
   <div class="row subMenu" id="theMenu2">
 
-    <div class="col-md-4"><a href="/customers">Customer</a></div>
-    <div class="col-md-4"><a href="/tyres">Tyre</a></div>
-    <div class="col-md-4"><a href="/consignments">Consignments</a></div>
-    <div class="col-md-4"><a href="/lcs">LCs</a></div>
-    <div class="col-md-4"><a href="/consignment_expenses">Expenses</a></div>
-    <div class="col-md-4"><a href="/orders">Order</a></div>
-    <div class="col-md-4"><a href="/payments">Payment</a></div>
-    <div class="col-md-4"><a href="/stock">Stock</a></div>
+    <div class="col-md-3">
+      <a href="/customers">
+        <h5>
+          <span><img src="/images/user.png" width="45" height="45"></span>
+          <strong>Customer</strong>
+        </h5>
+      </a>
+    </div>
 
-    <div class="col-md-4"><a href="/consignment_containers">consignment_containers</a></div>
+    <div class="col-md-3">
+      <a href="/tyres">
+        <h5>
+          <span><img src="/images/tyre.png" width="32" height="32"></span>
+          <strong>Tyre</strong>
+        </h5>
+      </a>
+    </div>
+
+    <div class="col-md-3">
+      <a href="/consignments">
+        <h5>
+          <span><img src="/images/consignment.png" width="45" height="45"></span>
+          <strong>Consignments</strong>
+        </h5>
+      </a>
+    </div>
+
+    <div class="col-md-3">
+      <a href="/lcs">
+        <h5>
+          <span><img src="/images/lc.png" width="45" height="45"></span>
+          <strong>LCs</strong>
+        </h5>
+      </a>
+    </div>
+
+    <div class="col-md-3">
+      <a href="/consignment_expenses">
+        <h5>
+          <span><img src="/images/expense.png" width="45" height="45"></span>
+          <strong>Expenses</strong>
+        </h5>
+      </a>
+    </div>
+
+    <div class="col-md-3">
+      <a href="/orders">
+        <h5>
+          <span><img src="/images/order.png" width="40" height="40"></span>
+          <strong>Order</strong>
+        </h5>
+      </a>
+    </div>
+
+    <div class="col-md-3">
+      <a href="/payments">
+        <h5>
+          <span><img src="/images/cash.png" width="50" height="50"></span>
+          <strong>Payment</strong>
+        </h5>
+      </a>
+    </div>
+
+    <div class="col-md-3">
+      <a href="/stock">
+        <h5>
+          <span><img src="/images/stock.png" width="40" height="40"></span>
+          <strong>Stock</strong>
+        </h5>
+      </a>
+    </div>
+
+    {{--<div class="col-md-4">
+      <a href="/consignment_containers">
+        consignment_containers
+      </a>
+    </div> --}}
 
   </div>
 
