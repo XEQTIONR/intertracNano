7963495821b6b2caf058f2ccd7677d657740f5a6
Ishtehar Hussain <ishteharhussain@gmail.com>
2017-07-08 00:09:11 -0400

    CSS layout off 'Perform Actions' menu

--- resources/views/partials/mainmenu.blade.php
+++ resources/views/partials/mainmenu.blade.php
@@ -24,11 +24,11 @@
     <div class="col-md-3 vertical-spaced">
       <a href="/customers">
         <div class="row">
-          <div class="col-md-2">
+          <div class="col-md-2 col-sm-1 col-xs-1">
             <img src="/images/user.png" width="45" height="45">
           </div>
 
-          <div class="col-md-10">
+          <div class="col-md-10 col-sm-11 col-xs-11">
             <div class="row">
               <div class="col-md-12 menu-option-title">
                 Customers
@@ -48,11 +48,11 @@
     <div class="col-md-3 vertical-spaced">
       <a href="/tyres">
         <div class="row">
-          <div class="col-md-2" >
+          <div class="col-md-2 col-sm-1 col-xs-1" >
             <img src="/images/tyre.png" width="35 " height="35">
           </div>
 
-          <div class="col-md-10">
+          <div class="col-md-10 col-sm-11 col-xs-11">
             <div class="row">
               <div class="col-md-12 menu-option-title">
                 Tyres
@@ -72,11 +72,11 @@
     <div class="col-md-3 vertical-spaced">
       <a href="/consignments">
         <div class="row">
-          <div class="col-md-2">
+          <div class="col-md-2 col-sm-1 col-xs-1">
             <img src="/images/consignment.png" width="45" height="45">
           </div>
 
-          <div class="col-md-10">
+          <div class="col-md-10 col-sm-11 col-xs-11">
             <div class="row">
               <div class="col-md-12 menu-option-title">
                 Consignments
@@ -95,11 +95,11 @@
     <div class="col-md-3 vertical-spaced">
       <a href="/lcs">
         <div class="row">
-          <div class="col-md-2">
+          <div class="col-md-2 col-sm-1 col-xs-1">
             <img src="/images/lc.png" width="45" height="45">
           </div>
 
-          <div class="col-md-10">
+          <div class="col-md-10 col-sm-11 col-xs-11">
             <div class="row">
               <div class="col-md-12 menu-option-title">
                 LCs
@@ -119,11 +119,11 @@
     <div class="col-md-3 vertical-spaced">
       <a href="/consignment_expenses">
         <div class="row">
-          <div class="col-md-2">
+          <div class="col-md-2 col-sm-1 col-xs-1">
             <img src="/images/expense.png" width="40" height="40">
           </div>
 
-          <div class="col-md-10">
+          <div class="col-md-10 col-sm-11 col-xs-11">
             <div class="row">
               <div class="col-md-12 menu-option-title">
                 Expenses
@@ -143,11 +143,11 @@
     <div class="col-md-3 vertical-spaced">
       <a href="/orders">
         <div class="row">
-          <div class="col-md-2">
+          <div class="col-md-2 col-sm-1 col-xs-1">
             <img src="/images/order.png" width="40" height="40">
           </div>
 
-          <div class="col-md-10">
+          <div class="col-md-10 col-sm-11 col-xs-11">
             <div class="row">
               <div class="col-md-12 menu-option-title">
                 Orders
@@ -167,11 +167,11 @@
     <div class="col-md-3 vertical-spaced">
       <a href="/payments">
         <div class="row">
-          <div class="col-md-2">
+          <div class="col-md-2 col-sm-1 col-xs-1">
             <img src="/images/cash.png" width="48" height="48">
           </div>
 
-          <div class="col-md-10">
+          <div class="col-md-10 col-sm-11 col-xs-11">
             <div class="row">
               <div class="col-md-12 menu-option-title">
                 Payments
@@ -192,11 +192,11 @@
     <div class="col-md-3 vertical-spaced">
       <a href="/stock">
         <div class="row">
-          <div class="col-md-2">
+          <div class="col-md-2 col-sm-1 col-xs-1">
             <img src="/images/stock.png" width="40" height="40">
           </div>
 
-          <div class="col-md-10">
+          <div class="col-md-10 col-sm-11 col-xs-11">
             <div class="row">
               <div class="col-md-12 menu-option-title">
                 Stock
@@ -226,20 +226,239 @@
 
   <div class="row subMenu" id="theMenu3">
 
-    <div class="col-md-4"><a href="/tyres/create">Add a new Tyre</a></div>
-    <div class="col-md-4"><a href="/lcs/create">Add new LC</a></div>
-    <div class="col-md-4"><a href="/performa_invoices/create">Add new Performa Invoice</a></div>
-    <div class="col-md-4"><a href="/consignments/create">Add new consignments</a></div>
-    <div class="col-md-4"><a href="/consignment_expenses/create">Add new expense</a></div>
+    <div class="col-md-3 vertical-spaced">
+      <a href="/tyres/create">
+        <div class="row">
+          <div class="col-md-2 col-sm-1 col-xs-1" >
+            <img src="/images/tyre.png" width="35 " height="35">
+          </div>
 
-    <div class="col-md-4"><a href="/customers/create">Add a Customer</a></div>
-    <div class="col-md-4"><a href="/orders/create">Create new Order</a></div>
-    <div class="col-md-4"><a href="/payments/create">Create new Payment Invoice</a></div>
+          <div class="col-md-10 col-sm-11 col-xs-11">
+            <div class="row">
+              <div class="col-md-12 menu-option-title">
+                Add a new Tyre
+              </div>
+            </div> <!--row-->
 
-    <div class="col-md-4"><a href="/consignment_containers/create">Add containers to Consignment</a></div>
-    <div class="col-md-4"><a href="/container_contents/create">New Commercial Invoice</a></div>
-    <div class="col-md-4"><a href="/hscodes/create">Create a new Hscode</a></div>
+            <div class="row">
+              <div class="col-md-12">
+                Add a new kind of tyre to tyre catalog
+              </div>
+            </div>
+          </div>
+        </div>
+      </a>
+    </div>
+    <div class="col-md-3 vertical-spaced">
+      <a href="/lcs/create">
+        <div class="row">
+          <div class="col-md-2 col-sm-1 col-xs-1">
+            <img src="/images/lc.png" width="45" height="45">
+          </div>
 
+          <div class="col-md-10 col-sm-11 col-xs-11">
+            <div class="row">
+              <div class="col-md-12 menu-option-title">
+                Add a new LC
+              </div>
+            </div>
+
+            <div class="row">
+              <div class="col-md-12">
+                Add a new approved Letter of Credit to the database.
+              </div>
+            </div>
+          </div>
+        </div> <!--row-->
+      </a>
+    </div>
+    <div class="col-md-3 vertical-spaced">
+      <a href="/performa_invoices/create">
+        <div class="row">
+          <div class="col-md-2 col-sm-1 col-xs-1">
+            <img src="/images/performa-invoice.png" width="45" height="45">
+          </div>
+
+          <div class="col-md-10 col-sm-11 col-xs-11">
+            <div class="row">
+              <div class="col-md-12 menu-option-title">
+                Add a new performa invoice
+              </div>
+            </div>
+
+            <div class="row">
+              <div class="col-md-12">
+                The draft invoice provided to the bank when creating a LC
+              </div>
+            </div>
+          </div>
+        </div> <!--row-->
+      </a>
+    </div>
+    <div class="col-md-3 vertical-spaced">
+      <a href="/consignments/create">
+        <div class="row">
+          <div class="col-md-2 col-sm-1 col-xs-1">
+            <img src="/images/consignment.png" width="45" height="45">
+          </div>
+
+          <div class="col-md-10 col-sm-11 col-xs-11">
+            <div class="row">
+              <div class="col-md-12 menu-option-title">
+                Add a new Consignment
+              </div>
+            </div>
+            <div class="row">
+              <div class="col-md-12">
+                Add a new consignment to the database against a LC
+              </div>
+            </div>
+          </div>
+        </div> <!--row-->
+      </a>
+    </div>
+    <div class="col-md-3 vertical-spaced">
+      <a href="/consignment_expenses/create">
+        <div class="row">
+          <div class="col-md-2 col-sm-1 col-xs-1">
+            <img src="/images/expense.png" width="40" height="40">
+          </div>
+
+          <div class="col-md-10 col-sm-11 col-xs-11">
+            <div class="row">
+              <div class="col-md-12 menu-option-title">
+                Add a new Expense
+              </div>
+            </div> <!--row-->
+
+            <div class="row">
+              <div class="col-md-12">
+                Add a new expense for a consignment.
+              </div>
+            </div>
+          </div>
+        </div> <!--row-->
+      </a>
+    </div>
+
+    <div class="col-md-3 vertical-spaced">
+      <a href="/customers/create">
+        <div class="row">
+          <div class="col-md-2 col-sm-1 col-xs-1">
+            <img src="/images/user.png" width="45" height="45">
+          </div>
+
+          <div class="col-md-10 col-sm-11 col-xs-11">
+            <div class="row">
+              <div class="col-md-12 menu-option-title">
+                Add a customer
+              </div>
+            </div>
+
+            <div class="row">
+              <div class="col-md-12">
+              Add a new customer who can order from us
+              </div>
+            </div>
+          </div>
+        </div> <!--row-->
+      </a>
+    </div>
+    <div class="col-md-3 vertical-spaced">
+      <a href="/orders/create">
+        <div class="row">
+          <div class="col-md-2 col-sm-1 col-xs-1">
+            <img src="/images/order.png" width="40" height="40">
+          </div>
+
+          <div class="col-md-10 col-sm-11 col-xs-11">
+            <div class="row">
+              <div class="col-md-12 menu-option-title">
+                Create a new order
+              </div>
+            </div>
+
+            <div class="row">
+              <div class="col-md-12">
+                Record a new order placed by a customer.
+              </div>
+            </div>
+          </div>
+        </div> <!--row-->
+      </a>
+    </div>
+    <div class="col-md-3 vertical-spaced">
+      <a href="/payments/create">
+        <div class="row">
+          <div class="col-md-2 col-sm-1 col-xs-1">
+            <img src="/images/cash.png" width="48" height="48">
+          </div>
+
+          <div class="col-md-10 col-sm-11 col-xs-11">
+            <div class="row">
+              <div class="col-md-12 menu-option-title">
+                Record a payment made
+              </div>
+            </div>
+
+            <div class="row">
+              <div class="col-md-12">
+                Create a payment invoice for a new payment
+              </div>
+            </div>
+          </div>
+        </div> <!--row-->
+      </a>
+    </div>
+
+    <div class="col-md-3 vertical-spaced">
+      <a href="/consignment_containers/create">
+        <div class="row">
+          <div class="col-md-2 col-sm-1 col-xs-1">
+            <img src="/images/container.png" width="45" height="45">
+          </div>
+
+          <div class="col-md-10 col-sm-11 col-xs-11">
+            <div class="row">
+              <div class="col-md-12 menu-option-title">
+                Add a new container
+              </div>
+            </div>
+
+            <div class="row">
+              <div class="col-md-12">
+                Add a new container arrived to an existing consignment recorded
+              </div>
+            </div>
+          </div>
+        </div> <!--row-->
+      </a>
+    </div>
+    <div class="col-md-3 vertical-spaced">
+      <a href="/container_contents/create">
+        <div class="row">
+          <div class="col-md-2 col-sm-1 col-xs-1">
+            <img src="/images/commercial-invoice.png" width="35" height="35">
+          </div>
+
+          <div class="col-md-10 col-sm-11 col-xs-11">
+            <div class="row">
+              <div class="col-md-12 menu-option-title">
+                Add a new commercial invoice
+              </div>
+            </div>
+
+            <div class="row">
+              <div class="col-md-12">
+                Record information about goods from new containers
+              </div>
+            </div>
+          </div>
+        </div> <!--row-->
+      </a>
+    </div>
+    <!--<div class="col-md-4"><a href="/hscodes/create">Create a new Hscode</a></div>-->
+    {{--make hscode a field--}}
   </div>
 
 </div> <!--container fluid-->
