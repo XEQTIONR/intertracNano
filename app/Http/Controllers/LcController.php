592834f3f4a54f452f0dbec21820dba64a8f2018
Ishtehar Hussain <ishteharhussain@gmail.com>
2017-07-16 16:32:19 -0400

    Removed tha admin middleware form LcController.

--- app/Http/Controllers/LcController.php
+++ app/Http/Controllers/LcController.php
@@ -14,7 +14,6 @@ class LcController extends Controller
     public function __construct()
     {
       $this->middleware('auth');
-      $this->middleware('admin');
     }
     /**
      * Display a listing of the resource.
