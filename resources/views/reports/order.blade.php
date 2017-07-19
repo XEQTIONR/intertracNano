79c8efac9c9fea403a4f59d8bdcf04846bd114b5
Ishtehar Hussain <ishteharhussain@gmail.com>
2017-07-18 23:54:10 -0400

    Added a form to pick a quarter month or year for which to generate the report. Some css styling as well.

--- resources/views/reports/order.blade.php
+++ resources/views/reports/order.blade.php
@@ -5,6 +5,11 @@
   <script src="/js/dough.js" type="text/javascript"></script>
   <script src="https://d3js.org/d3.v3.min.js" charset="utf-8"></script>
 
+
+
+
+
+
   <style>
   path.color0 {
       fill: teal;  /*filled section color*/
@@ -23,11 +28,57 @@
 
 @section('content')
 
-<div class="container-fluid">
+<div class="container">
 <div class="row">
   <div class="col-md-10 col-md-offset-1">
     <div class="page-header">
       <h1>Order Reports</h1>
+    </div>
+  </div>
+</div>
+
+<div class="row">
+  <div class="col-md-12">
+  <form class="form-inline">
+  <div class="row">
+  <div class="col-md-6">
+  <div class="form-group">
+    <label for="month">Monthly Report For</label>
+    <input type="text" class="form-control" id="month" placeholder="Report for Month">
+  </div>
+  </div>
+  <div class="col-md-6">
+  <div class="form-group">
+    <label for="year">or Yearly Report For</label>
+    <input type="text" class="form-control" id="year" placeholder="Report for Year">
+  </div>
+  </div>
+  </div>
+  <div class="row">
+  <div class="col-md-6">
+  <div class="form-group">
+    <label for="quarter"> or Quarterly Report For</label>
+    <input type="number" class="form-control" id="quarter" min="1" max=4 placeholder="1-4">
+  </div>
+  <div class="form-group">
+    <label for="qyear">of Year</label>
+    <input type="text" class="form-control" id="qyear" placeholder="Report for Year">
+  </div>
+  </div>
+  </div>
+  <div class="row">
+  <div class="col-md-6">
+    <button>Generate Report</button>
+  </div>
+  </div>
+  </form>
+  </div>
+</div>
+
+<div class="row">
+  <div class="col-md-12">
+    <div class="white-card">
+      <h1 class="stat">ORDER REPORT FOR {{$time_frame}} OF {{$year}}</h1>
     </div>
   </div>
 </div>
@@ -82,6 +133,67 @@
 </div> <!--first-row-->
 
 <div class="row">
+  <div class="col-md-6">
+    <div class="white-card">
+      <div class="row">
+        <div id="myDiv2" class="col-md-2 stat-diagram">
+          <script>
+            @if ($count>0)
+              build({{$orders_with_payments*100.0/$count}}, 100, ".1%","#myDiv2");
+            @else
+              build(0, 100, ".1%","#myDiv2");
+            @endif
+          </script>
+        </div>
+        <div class="col-md-4 col-md-offset-4">
+          <div class="row marg-top">
+            <div class="col-md-12 stat stat-figure">
+              {{$orders_with_payments}}
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-md-12 stat stat-text">
+              ORDERS WITH PAYMENTS
+            </div>
+          </div>
+        </div>
+      </div>
+    </div><!--white-card-->
+  </div>
+
+  <div class="col-md-6">
+    <div class="white-card">
+      <div class="row">
+        <div id="myDiv" class="col-md-2 stat-diagram" {{--style="border: 2px solid red;"--}}>
+
+          <script>
+            @if ($count>0)
+              build({{$orders_full_paid*100.0/$count}}, 100, ".1%","#myDiv");
+            @else
+              build(0, 100, ".1%","#myDiv");
+            @endif
+          </script>
+        </div>
+        <div class="col-md-4 col-md-offset-4 ">
+          <div class="row marg-top">
+            <div class="col-md-12 stat stat-figure">
+              {{$orders_full_paid}}
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-md-12 stat stat-text">
+              ORDERS PAID IN FULL
+            </div>
+          </div>
+        </div>
+
+
+      </div> <!--row-->
+    </div> <!--white-card-->
+  </div>
+</div><!--row-->
+
+<div class="row">
       <div class="col-md-6">
         <div class="white-card">
           <div class="row">
@@ -113,66 +225,7 @@
       </div>
 </div> <!-- second  row-->
 
-<div class="row">
-  <div class="col-md-6">
-    <div class="white-card">
-      <div class="row">
-        <div id="myDiv2" class="col-md-2 stat-diagram">
-          <script>
-            @if ($count>0)
-              build({{$orders_with_payments*100.0/$count}}, 100, ".1%","#myDiv2");
-            @else
-              build(0, 100, ".1%","#myDiv2");
-            @endif
-          </script>
-        </div>
-        <div class="col-md-5 col-md-offset-2">
-          <div class="row" style="margin-top: 5em;">
-            <div class="col-md-12 stat stat-figure">
-              {{$orders_with_payments}}
-            </div>
-          </div>
-          <div class="row">
-            <div class="col-md-12 stat stat-text">
-              ORDERS WITH PAYMENTS
-            </div>
-          </div>
-        </div>
-      </div>
-    </div><!--white-card-->
-  </div>
 
-  <div class="col-md-6">
-    <div class="white-card">
-      <div class="row">
-        <div id="myDiv" class="col-md-2 stat-diagram" {{--style="border: 2px solid red;"--}}>
-
-          <script>
-            @if ($count>0)
-              build({{$orders_full_paid*100.0/$count}}, 100, ".1%","#myDiv");
-            @else
-              build(0, 100, ".1%","#myDiv");
-            @endif
-          </script>
-        </div>
-        <div class="col-md-5 col-md-offset-2">
-          <div class="row" style="margin-top: 5em;">
-            <div class="col-md-12 stat stat-figure">
-              {{$orders_full_paid}}
-            </div>
-          </div>
-          <div class="row">
-            <div class="col-md-12 stat stat-text">
-              ORDERS PAID IN FULL
-            </div>
-          </div>
-        </div>
-
-
-      </div> <!--row-->
-    </div> <!--white-card-->
-  </div>
-</div><!--row-->
 
 </div><!--container-->
 
