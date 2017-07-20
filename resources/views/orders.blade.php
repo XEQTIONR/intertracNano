f55360a9ed007a4e273c298fb829419153e79ae4
Ishtehar Hussain <ishteharhussain@gmail.com>
2017-07-20 13:28:19 -0400

    The all-orders info is now displayed by the partial file.

--- resources/views/orders.blade.php
+++ resources/views/orders.blade.php
@@ -16,34 +16,7 @@
   </div>
 
 
-<table class="table table-hover table-condensed">
-<thead>
-  <tr>
-    <th>Order#</th>
-    <th>Customer ID</th>
-    <th>Discount%</th>
-    <th>Discount Amount</th>
-    <th>Tax Percentage</th>
-    <th>Tax Amount</th>
-    <th>Created</th>
-    <th>Updated</th>
-  </tr>
-</thead>
-<tbody>
-  @foreach ($orders as $order)
-    <tr style="cursor: pointer;" onclick="location.href='/orders/{{$order->Order_num}}'">
-      <td>{{$order->Order_num}}</td>
-      <td>{{$order->customer_id}}</td>
-      <td>{{$order->discount_percent}}</td>
-      <td>{{$order->discount_amount}}</td>
-      <td>{{$order->tax_percentage}}</td>
-      <td>{{$order->tax_amount}}</td>
-      <td>{{$order->created_at}}</td>
-      <td>{{$order->updated_at}}</td>
-    </tr>
-  @endforeach
-</tbody>
-</table>
+@include('partials.tables.orders')
 
 </div>
 
