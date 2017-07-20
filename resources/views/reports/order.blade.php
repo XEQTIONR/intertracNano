197f563d1391a8e0b1d17fe80b249d344dc6db6f
Ishtehar Hussain <ishteharhussain@gmail.com>
2017-07-20 12:13:19 -0400

    Moved the matched orders section/table to a partial file.

--- resources/views/reports/order.blade.php
+++ resources/views/reports/order.blade.php
@@ -234,34 +234,7 @@
   <div class="col-md-12">
   <div class="white-card">
 
-    <table class="table table-hover table-condensed">
-    <thead>
-      <tr>
-        <th>Order#</th>
-        <th>Customer ID</th>
-        <th>Discount%</th>
-        <th>Discount Amount</th>
-        <th>Tax Percentage</th>
-        <th>Tax Amount</th>
-        <th>Created</th>
-        <th>Updated</th>
-      </tr>
-    </thead>
-    <tbody>
-      @foreach ($orders as $order)
-        <tr style="cursor: pointer;" onclick="location.href='/orders/{{$order->Order_num}}'">
-          <td>{{$order->Order_num}}</td>
-          <td>{{$order->customer_id}}</td>
-          <td>{{$order->discount_percent}}</td>
-          <td>{{$order->discount_amount}}</td>
-          <td>{{$order->tax_percentage}}</td>
-          <td>{{$order->tax_amount}}</td>
-          <td>{{$order->created_at}}</td>
-          <td>{{$order->updated_at}}</td>
-        </tr>
-      @endforeach
-    </tbody>
-    </table>
+    @include('partials.orders')
 
   </div>
   </div>
