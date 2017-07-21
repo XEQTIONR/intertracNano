99521a251b8c20e6c42d801a55e6ee449aed48e0
Ishtehar Hussain <ishteharhussain@gmail.com>
2017-07-20 20:54:55 -0400

    Using the payment table partial in payment reports.

--- resources/views/reports/payment.blade.php
+++ resources/views/reports/payment.blade.php
@@ -6,28 +6,6 @@
   <script src="https://d3js.org/d3.v3.min.js" charset="utf-8"></script>
 
   <script src="/js/reports.js" type="text/javascript"></script>
-
-
-
-
-<style>
-
-  .ui-datepicker-calendar{
-    display: none;
-  }
-  path.color0 {
-      fill: teal;  /*filled section color*/
-  }
-  path.color1 {
-      fill: #AAA; /*unfilled section color*/
-  }
-  text {
-      font-size: 2em;
-      font-weight: 400;
-      line-height: 16em;
-      fill: teal;
-  }
-</style>
 @endsection
 
 @section('content')
@@ -122,63 +100,11 @@
   </div> <!--first-row-->
 
   <div class="row">
-    <table class="table table-condensed">
-    <thead>
-      <tr>
-        <th>Invoice#</th>
-        <th>Order#</th>
-        <th>Amount Paid</th>
-        <th>Created</th>
-        <th>Updated</th>
-      </tr>
-    </thead>
-    <tbody>
-      @foreach ($payments as $payment)
-        <tr>
-          <td>{{$payment->Invoice_num}}</td>
-          <td><a href="orders/{{$payment->Order_num}}">{{$payment->Order_num}}</a></td>
-          <td>{{$payment->payment_amount}}</td>
-          <td>{{$payment->created_at}}</td>
-          <td>{{$payment->updated_at}}</td>
-
-        </tr>
-      @endforeach
-    </tbody>
-    </table>
+    <div class="col-md-12">
+      <div class="white-card">
+        @include('partials.tables.payments')
+      </div>
+    </div>
   </div>
-
-
-
 </div><!--container-->
-
-  {{--  <div class="row">
-      <div class="col-md-6 white-card">
-
-        Report generated on:{{$date}}<br>
-        Report for {{$time_frame}} year {{$year}}<br>
-        1.No of order: {{$count}}<br>
-        2.Total tyres sold: {{$count_tyres}}<br>
-        3.Total Value: {{$total_value}}<br>
-        4.Avg no of tyres in each order: {{$avg_tyre}}<br>
-        5.Avg Value per order: {{$avg_value}}<br>
-        6.No of order with payment: {{$orders_with_payments}}<br>
-        7.Orders fully paid: {{$orders_full_paid}}
-
-      </div>
-
-      <div class="col-md-6">
-        <div class="white-card">
-        Report generated on:{{$date}}<br>
-        Report for {{$time_frame}} year {{$year}}<br>
-        1.No of order: {{$count}}<br>
-        2.Total tyres sold: {{$count_tyres}}<br>
-        3.Total Value: {{$total_value}}<br>
-        4.Avg no of tyres in each order: {{$avg_tyre}}<br>
-        5.Avg Value per order: {{$avg_value}}<br>
-        6.No of order with payment: {{$orders_with_payments}}<br>
-        7.Orders fully paid: {{$orders_full_paid}}
-        </div>
-      </div>
-
-  </div>  --}}
 @endsection
