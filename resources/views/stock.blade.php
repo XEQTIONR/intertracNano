89ac4070ea13ec35ae98824397b6039b6056fa7d
Ishtehar Hussain <x.e.q.tionrz@gmail.com>
2019-04-22 11:56:20 -0400

    view stock update

--- resources/views/stock.blade.php
+++ resources/views/stock.blade.php
@@ -21,24 +21,24 @@
       <table id ="table_id" class="table table-hover table-bordered">
         <thead>
         <tr>
-          <th> Tyre ID </th>
-          <th> Brand </th>
-          <th> Size </th>
-          <th> Pattern </th>
-          <th> LiSi </th>
-          <td># in stock</td>
+          <th class=" col-xs-1"> Tyre ID </th>
+          <th class=" col-xs-3"> Brand </th>
+          <th class=" col-xs-2"> Size </th>
+          <th class=" col-xs-2"> Pattern </th>
+          <th class=" col-xs-2"> LiSi </th>
+          <th class=" col-xs-2"># in stock</th>
         </tr>
         </thead>
         <tbody>
 
         @foreach($in_stock as $item)
         <tr>
-          <td class="text-center">{{$item->tyre_id}}</td>
-          <td class="text-center">{{$item->brand}}</td>
-          <td class="text-center">{{$item->size}}</td>
-          <td class="text-center">{{$item->pattern}}</td>
-          <td class="text-center">{{$item->lisi}}</td>
-          <td class="text-center">{{$item->in_stock}}</td>
+          <td class="text-center col-xs-1">{{$item->tyre_id}}</td>
+          <td class="text-center col-xs-3">{{$item->brand}}</td>
+          <td class="text-center col-xs-2">{{$item->size}}</td>
+          <td class="text-center col-xs-2">{{$item->pattern}}</td>
+          <td class="text-center col-xs-2">{{$item->lisi}}</td>
+          <td class="text-center col-xs-2">{{$item->in_stock}}</td>
         </tr>
         @endforeach
         </tbody>
