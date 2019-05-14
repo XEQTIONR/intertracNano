a6ea727ee54996eed258db03ce7aa93939b6f76e
Ishtehar Hussain <x.e.q.tionrz@gmail.com>
2019-05-14 11:15:20 -0400

    Customer seeder

---
+++ database/seeds/CustomerTableSeeder.php
@@ -0,0 +1,18 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Customer;
+
+class CustomerTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+      $people = factory(Customer::class, 10)->create();
+    }
+}
