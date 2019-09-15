82aeff575fafd1a2a4958a5f358fbdff6aa2b542
Ishtehar Hussain <x.e.q.tionrz@gmail.com>
2019-09-15 16:33:18 -0400

    NoPayments middleware

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -57,5 +57,6 @@ class Kernel extends HttpKernel
         'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'admin' => \App\Http\Middleware\CheckAdmin::class,
+        'no_payments' => \App\Http\Middleware\NoPayments::class
     ];
 }
