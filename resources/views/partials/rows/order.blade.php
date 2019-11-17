9c26aa3ada856d823280b54d499e9aaa6b031ed8
Ishtehar Hussain <x.e.q.tionrz@gmail.com>
2019-11-17 09:11:42 -0500

    Adding return date in return table in order row

--- resources/views/partials/rows/order.blade.php
+++ resources/views/partials/rows/order.blade.php
@@ -150,7 +150,8 @@
       <thead>
       <tr>
         <th class="col-xs-1">#</th>
-        <th class="col-xs-5">Tyre</th>
+        <th class="col-xs-3">Tyre</th>
+        <th class="col-xs-2">Return Date</th>
         <th class="col-xs-2">Qty</th>
         <th class="col-xs-2">Unit Price</th>
         <th class="col-xs-2 text-right">Subtotal</th>
@@ -162,7 +163,8 @@
         <?php $i++ ?>
         <tr>
           <td class="col-xs-1">{{ $i }}</td>
-          <td class="col-xs-5"><b>({{ $item->tyre->tyre_id }})</b> {{ $item->tyre->brand }} {{ $item->tyre->size }} {{ $item->tyre->pattern }} {{ $item->tyre->lisi }}</td>
+          <td class="col-xs-3"><b>({{ $item->tyre->tyre_id }})</b> {{ $item->tyre->brand }} {{ $item->tyre->size }} {{ $item->tyre->pattern }} {{ $item->tyre->lisi }}</td>
+          <td class="col-xs-2">{{ $item->created_at }}</td>
           <td class="col-xs-2">{{ $item->qty }}</td>
           <td class="col-xs-2">৳ {{ $item->unit_price }}</td>
           {{--<td class="col-xs-2"> @{{ parseFloat(item.unit_price)* parseInt(item.qty) / parseFloat(subTotal) |percentage_rounded}}</td>--}}
@@ -172,6 +174,7 @@
       <tr>
         <td></td>
         <td><b>Total</b></td>
+        <td></td>
         <td><b>{{$order->qtytotalReturn}}</b></td>
         <td></td>
         <td class="text-right"><b>৳ {{number_format($order->subtotalReturn, 2)}}</b></td>
@@ -180,6 +183,7 @@
       <tr>
         <td></td>
         <td><b>Discount @if($order->discount_percent>0)<span class="ml-2">({{$order->discount_percent}} %)</span>@endif</b></td>
+        <td></td>
         <td></td>
         <td></td>
         <td class="text-right"><b>৳ {{number_format($order->discounttotalReturn, 2)}}</b></td>
@@ -191,12 +195,14 @@
         <td><b>Tax  @if($order->tax_percentage>0)<span class="ml-2">({{$order->tax_percentage}} %)</span>@endif</b></td>
         <td></td>
         <td></td>
+        <td></td>
         <td class="text-right"><b>৳ {{number_format($order->taxtotalReturn, 2)}}</b></td>
       </tr>
       @endif
       <tr>
         <td></td>
         <td class="text-uppercase"><b>Grand Total</b></td>
+        <td></td>
         <td></td>
         <td></td>
         <td class="text-right"><b>৳ {{number_format($order->grandtotalReturn, 2)}}</b></td>
