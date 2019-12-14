64efb4177f4c7b44c479dded361c7b685eb15bb3
Ishtehar Hussain <x.e.q.tionrz@gmail.com>
2019-12-14 07:25:04 -0500

    Waste form updates

--- resources/views/waste.blade.php
+++ resources/views/waste.blade.php
@@ -16,19 +16,23 @@
 @endsection
 
 @section('body')
-  <div class="row">
-    <div class="col-xs-12">
-    <h3 class="header"><i class="icon-tire-flat"></i> Add items that have perished.</h3>
+  <div class="row justify-content-center">
+    <div class="col-xs-12 col-sm-11 col-lg-8">
+      <div class="callout callout-danger">
+{{--        <div class="box-header">--}}
+          <h4 class="mt-5"><i class="icon-tire-flat mr-2" style="position: relative; top: 1px"></i> Add items that have perished.</h4>
+{{--        </div>--}}
+      </div>
     </div>
   </div>
-  <div v-cloak class="row" v-for="(consignment,index) in remain">
-    <div class="col-xs-12 col-lg-8" v-for="(contents, bol) in consignment">
+  <div  v-cloak class="row justify-content-center" v-for="(consignment,index) in remain" v-if="step ==0 || step==1 && consignmentWasteTotal(consignment) > 0">
+    <div class="col-xs-11 col-lg-8" v-for="(contents, bol) in consignment">
       <div class="box">
         <div class="box-header">
           <h3 class="box-title"><b>Bill Of Lading # </b>@{{ bol }}</h3>
         </div>
         <div class="box-body">
-          <table class="table table-" v-for="(stock,container_num) in contents" style="margin-bottom: 5px !important;">
+          <table class="table" v-for="(stock,container_num) in contents" v-if="step==0 || step==1 && containerWasteTotal(bol, container_num) > 0" style="margin-bottom: 5px !important;" >
           <thead>
             <tr>
               <th colspan="4">
@@ -45,16 +49,17 @@
           </thead>
           <tbody>
 
-            <tr v-for="(item, idx) in stock">
+            <tr v-for="(item, idx) in stock"  v-if="step==0 || step==1 && item.ret_amt > 0">
               <td class="col-xs-6"><b>(@{{ item.tyre_id }})</b> @{{ item.tyre.brand }} @{{ item.tyre.size }} @{{ item.tyre.pattern }} @{{ item.tyre.lisi }}</td>
               <td class="col-xs-2 text-center">@{{ item.in_stock }}</td>
-              <td class="col-xs-2"><input class="text-center"   v-model="remain[index][bol][container_num][idx].ret_amt"> </td>
+              <td v-if="step==0" class="col-xs-2"><input class="text-center"   v-model="remain[index][bol][container_num][idx].ret_amt"> </td>
+              <td v-if="step==1" class="col-xs-2"><span class="text-center d-block mx-auto"   v-text="remain[index][bol][container_num][idx].ret_amt"></span> </td>
               <td class="col-xs-2 text-red text-center" v-if="parseInt(remain[index][bol][container_num][idx].ret_amt)!=0">@{{ parseInt(item.in_stock) - parseInt(remain[index][bol][container_num][idx].ret_amt) }} </td>
             </tr>
           </tbody>
           <tfoot>
             <tr>
-              <td class="col-xs-6 strong">Container Totals</td>
+              <td class="col-xs-6 strong">Container Totals <small v-if="step==1">(Changed Only)</small></td>
               <td class="col-xs-2 strong text-center">@{{ containerTotal(bol, container_num) }}</td>
               <td class="col-xs-2 strong text-center">@{{ containerWasteTotal(bol, container_num) }}</td>
               <td class="col-xs-2 text-red text-center strong" v-if="parseInt(containerTotal(bol, container_num))!=parseInt(containerTotalAfterReturn(bol, container_num))">@{{ containerTotalAfterReturn(bol, container_num) }}</td>
@@ -64,35 +69,67 @@
         </div>
       </div>
     </div>
-{{--      @foreach($remain as $consignment)--}}
-{{--        @foreach($consignment as $bol => $containers)--}}
-{{--        <h4>{{$bol}}</h4><br>--}}
-{{--          <table class="table table-bordered">--}}
-{{--            @foreach($containers as $container_num => $contents)--}}
-{{--              <tr><td colspan="4"> {{$container_num}}</td></tr>--}}
-{{--              <tr>--}}
-{{--              @foreach($contents[0] as $key => $val)--}}
-{{--                <td>{{$key}}</td>--}}
-{{--              @endforeach--}}
-{{--              </tr>--}}
-{{--              @foreach($contents as $content)--}}
-{{--                <tr>--}}
-{{--                @foreach($content as $key => $val)--}}
-{{--                  <td>{{$val}}</td>--}}
-{{--                @endforeach--}}
-{{--                </tr>--}}
-{{--              @endforeach--}}
-{{--            @endforeach--}}
-{{--          </table>--}}
-{{--        @endforeach--}}
-{{--      @endforeach--}}
 
 
 
 
 
   </div>
+{{--  <div v-if="step==1" v-cloak class="row justify-content-center" v-for="(consignment,index) in filtered">--}}
+{{--    <div class="col-xs-11 col-lg-8" v-for="(contents, bol) in consignment">--}}
+{{--      <div class="box">--}}
+{{--        <div class="box-header">--}}
+{{--          <h3 class="box-title"><b>Bill Of Lading # </b>@{{ bol }}</h3>--}}
+{{--        </div>--}}
+{{--        <div class="box-body">--}}
+{{--          <table class="table table-" v-for="(stock,container_num) in contents" style="margin-bottom: 5px !important;">--}}
+{{--            <thead>--}}
+{{--            <tr>--}}
+{{--              <th colspan="4">--}}
+{{--                <i class="icon-container-storage-r fa-container-storage mr-3" style="position: relative; top: 3px; font-size : 1.5rem"></i>--}}
+{{--                # : @{{ container_num }}--}}
+{{--              </th>--}}
+{{--            </tr>--}}
+{{--            <tr>--}}
+{{--              <th class="col-xs-6">Tyre</th>--}}
+{{--              <th class="col-xs-2 text-center">in_stock</th>--}}
+{{--              <th class="col-xs-2 text-center">add waste</th>--}}
+{{--              <th class="col-xs-2 text-center text-red">Updated Stock</th>--}}
+{{--            </tr>--}}
+{{--            </thead>--}}
+{{--            <tbody>--}}
 
+{{--            <tr v-for="(item, idx) in stock">--}}
+{{--              <td class="col-xs-6"><b>(@{{ item.tyre_id }})</b> @{{ item.tyre.brand }} @{{ item.tyre.size }} @{{ item.tyre.pattern }} @{{ item.tyre.lisi }}</td>--}}
+{{--              <td class="col-xs-2 text-center">@{{ item.in_stock }}</td>--}}
+{{--              <td class="col-xs-2"><input class="text-center"   v-model="remain[index][bol][container_num][idx].ret_amt"> </td>--}}
+{{--              <td class="col-xs-2 text-red text-center" v-if="parseInt(remain[index][bol][container_num][idx].ret_amt)!=0">@{{ parseInt(item.in_stock) - parseInt(remain[index][bol][container_num][idx].ret_amt) }} </td>--}}
+{{--            </tr>--}}
+{{--            </tbody>--}}
+{{--            <tfoot>--}}
+{{--            <tr>--}}
+{{--              <td class="col-xs-6 strong">Container Totals</td>--}}
+{{--              <td class="col-xs-2 strong text-center"></td>--}}
+{{--              <td class="col-xs-2 strong text-center"></td>--}}
+{{--              <td class="col-xs-2 text-red text-center strong"></td>--}}
+{{--            </tr>--}}
+{{--            </tfoot>--}}
+{{--          </table>--}}
+{{--        </div>--}}
+{{--      </div>--}}
+{{--    </div>--}}
+
+
+
+
+
+{{--  </div>--}}
+  <div v-cloak class="row justify-content-center" v-if="change">
+    <div class="col-xs-12 col-sm-11 col-lg-8">
+      <button v-if="step == 1" class="btn btn-primary" @click="back()"><i class="fa fa-chevron-left mr-2"></i>Back  </button>
+      <button v-else class="btn btn-primary pull-right" @click="forward()">Continue <i class="fa fa-chevron-right ml-1"></i> </button>
+    </div>
+  </div>
 
 @endsection
 
@@ -108,8 +145,8 @@
         Object.keys(remain[i]).forEach(function(key){
             Object.keys(remain[i][key]).forEach(function(cont){
                 for(var j=0; j<remain[i][key][cont].length; j++){
-
                     remain[i][key][cont][j].ret_amt = 0;
+                    remain[i][key][cont][j].is_set = false;
                 }
             });
         });
@@ -117,8 +154,12 @@
 
     var app = new Vue({
         el: '#app',
-        data : {
-            remain : remain
+        data :function() {
+            return {
+                remain: remain,
+                change: false,
+                step: 0
+            }
         },
 
         watch : {
@@ -128,9 +169,7 @@
 
                 handler : function(new_val){
 
-
-                    console.log("REMAIN CHANGED");
-                    //console.log(new_val);
+                    var change =  false;
 
                     for(var i=0;i<new_val.length; i++){
                         Object.keys(new_val[i]).forEach(function(key){
@@ -144,79 +183,117 @@
                                         || (typeof new_val[i][key][cont][j].ret_amt == "string" && new_val[i][key][cont][j].ret_amt.indexOf(".")!= -1))
                                     {
                                         new_val[i][key][cont][j].ret_amt = 0;
+                                        new_val[i][key][cont][j].changed = false;
                                     }
                                     else if(typeof new_val[i][key][cont][j].ret_amt == "string" && new_val[i][key][cont][j].ret_amt[0] == "0")
                                     {
                                         new_val[i][key][cont][j].ret_amt = parseInt(new_val[i][key][cont][j].ret_amt);
+                                        new_val[i][key][cont][j].changed = true;
                                     }
+                                    else if(!isNaN(new_val[i][key][cont][j].ret_amt) && new_val[i][key][cont][j].ret_amt > 0)
+                                    {
+                                        change = true;
+                                        new_val[i][key][cont][j].changed = true;
+
+                                    }
+
+                                    if(typeof  new_val[i][key][cont][j].changed == 'undefined')
+                                    {
+                                        new_val[i][key][cont][j].changed = false;
+                                    }
+
                                     //remain[i][key][cont][j].ret_amt = 0;
                                 }
                             });
                         });
                     }
+                    app.change = change;
                 }
             }
         },
 
         methods : {
+            forward : function(){
+
+                window.scrollTo(0,0);
+                app.step = 1;
+            }
+            ,
+
+            back : function(){
+                app.step = 0;
+            },
+
             containerTotal : function(bol, container_num){
-                var ret = null;
+                var ret = 0;
+                var thestep = this.step; // cannot reference this directly inside callback
                 for(var i=0; i<this.remain.length; i++)
                 {
                     Object.keys(this.remain[i]).forEach(function(key){
-                        if(key==bol){
+                        if(key==bol && this.remain[i][bol]!=null)
                             Object.keys(this.remain[i][bol]).forEach(function(cont){
-                                if(cont==container_num)
+                                if(cont==container_num && this.remain[i][bol][container_num]!=null)
                                     for(var j=0; j<this.remain[i][bol][container_num].length; j++){
-                                        if(ret ==  null)
-                                            ret = 0;
-                                        ret += parseInt(this.remain[i][bol][container_num][j].in_stock);
+                                        // if(ret ==  null)
+                                        //     ret = 0;
+                                        if(thestep == 0 || thestep == 1 && parseInt(this.remain[i][bol][container_num][j].ret_amt)>0)
+                                          ret += parseInt(this.remain[i][bol][container_num][j].in_stock);
                                     }
                             })
-                        }
 
                     });
                 }
                 return ret;
             },
             containerTotalAfterReturn : function(bol, container_num){
-                var ret = null;
+                var ret = 0;
+                var thestep = this.step; // cannot reference this directly inside callback
                 for(var i=0; i<this.remain.length; i++)
                 {
                     Object.keys(this.remain[i]).forEach(function(key){
-                        if(key==bol){
+                        if(key==bol && this.remain[i][bol]!=null)
                             Object.keys(this.remain[i][bol]).forEach(function(cont){
-                                if(cont==container_num)
+                                if(cont==container_num && this.remain[i][bol][container_num]!=null)
                                     for(var j=0; j<this.remain[i][bol][container_num].length; j++){
                                         if(ret ==  null)
                                             ret = 0;
-                                        ret += (parseInt(this.remain[i][bol][container_num][j].in_stock) - parseInt(this.remain[i][bol][container_num][j].ret_amt));
+                                        if(thestep == 0 || thestep == 1 && parseInt(this.remain[i][bol][container_num][j].ret_amt)>0)
+                                          ret += (parseInt(this.remain[i][bol][container_num][j].in_stock) - parseInt(this.remain[i][bol][container_num][j].ret_amt));
                                     }
                             })
-                        }
-
                     });
                 }
                 return ret;
             },
             containerWasteTotal : function(bol, container_num){
-                var ret = null;
+                var ret = 0;
                 for(var i=0; i<this.remain.length; i++)
                 {
                     Object.keys(this.remain[i]).forEach(function(key){
-                        if(key==bol){
+                        if(key==bol && this.remain[i][bol]!=null)
                             Object.keys(this.remain[i][bol]).forEach(function(cont){
-                                if(cont==container_num)
+                                if(cont==container_num && this.remain[i][bol][container_num]!=null)
                                     for(var j=0; j<this.remain[i][bol][container_num].length; j++){
                                         if(ret ==  null)
                                             ret = 0;
                                         ret += parseInt(this.remain[i][bol][container_num][j].ret_amt);
                                     }
                             })
-                        }
 
                     });
                 }
+                return ret;
+            },
+
+            consignmentWasteTotal : function(consignment){
+                var ret = 0;
+                Object.keys(consignment).forEach(function(bol){
+                    Object.keys(consignment[bol]).forEach(function(cont_num){
+                        for(var i=0; i<consignment[bol][cont_num].length; i++)
+                            ret += parseInt(consignment[bol][cont_num][i].ret_amt);
+                    })
+                });
+
                 return ret;
             }
         },
